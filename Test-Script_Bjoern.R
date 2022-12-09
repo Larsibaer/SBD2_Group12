2249d0644a4fa3e520f116b980b899438f9dab26
DESKTOP-16C8CU5\bjoer <langb4@bfh.ch>
2022-12-09 12:11:18 +0100

    script functional

--- Test-Script_Bjoern.R
+++ Test-Script_Bjoern.R
@@ -18,7 +18,7 @@ data <- data.frame(loan)
 
 
 #----------------------------------------------------------------------------
-#--------- Exercise 1 -------------------------------------------------------
+#--------- Exploration -------------------------------------------------------
 #----------------------------------------------------------------------------
 
 
@@ -36,14 +36,16 @@ summary(data)
 overview <- overview(data)
 list(overview)
 
+#----------------------------------------------------------------------------
 #---------TARGET SAMPLE------------------------------------------------------
+#----------------------------------------------------------------------------
 
 #Target varible
 table(data$Status)
 barplot(table(data$Status))
 
 
-# equalizing the sample
+# Undersampling
 
 set.seed(7)
 data_balanced <- ovun.sample(Status ~ ., data=data, method = "under")
@@ -53,9 +55,9 @@ table(data_under$Status)
 barplot(table(data_under$Status))
 
 
-
-#---------OUTLIERS------------------------------------------------------
-
+#----------------------------------------------------------------------------
+#---------OUTLIERS-----------------------------------------------------------
+#----------------------------------------------------------------------------
 
 #Dimensions of new set
 dim(data_under)
@@ -64,12 +66,14 @@ dim(data_under)
 data_under_num <- data_under %>%
   select_if(is.numeric)
 data_under_num <- as.data.frame(data_under_num)
+dim(data_under_num)
 
 
 #Categorical Variables
 data_under_cat <- data_under %>%
   select_if(is.character)
 data_under_cat <- as.data.frame(data_under_cat)
+dim(data_under_cat)
 
 
 
@@ -115,9 +119,11 @@ outlier_remove <- function(x){
 }
 
 
-
+# ----Use this to clean up data set further: -------------------------------
 #diagnose_web_report(data_under_num)
 
+
+#----------CAPPING OUTLIERS--------------------------------
 #Lets cap and see:
 # Apply outlier function to data set
 data_under_num_capped <- map_df(data_under_num, outlier_cap)
@@ -140,7 +146,7 @@ data_under_capped <- cbind(data_under_num_capped, data_under_cat)
 
 
 
-
+#----------REMOVING OUTLIERS--------------------------------
 #Lets remove and compare:
 # Apply outlier function to data set
 data_under_num_removed <- map_df(data_under_num, outlier_remove)
@@ -169,8 +175,9 @@ imp_income <- imputate_outlier(data_under, annual_inc, method = "median")
 imp_income
 summary(imp_income)
 
-
-#---------FEATURES------------------------------------------------------
+#-----------------------------------------------------------------------
+#--------- FEATURES SELECTION ------------------------------------------
+#-----------------------------------------------------------------------
 
 #Dimensions of new sets
 dim(data_under_capped)
@@ -180,8 +187,10 @@ dim(data_under_removed)
 dim(data_under_num_removed)
 
 #shorter variable names
+#---------HERE CAPPED OUTLIERS WERE CHOSEN ---------------------------------
 clean <- data.frame(data_under_capped)
 clean_num <- data.frame(data_under_num_capped)
+dim(clean)
 
 #or go with outliers removed...
 #clean <- data_under_removed
@@ -189,49 +198,159 @@ clean_num <- data.frame(data_under_num_capped)
 
 
 
-#Correlation of numeric values to Status
-# ---- Code here ---- 
 
-
+#----------Correlation of numeric values to Status---------------------------
 corr <- cor(clean_num)
-corr
-
 ggcorrplot(corr)
-# --> no highly correlated data! thats good
-
-data_key <- clean %>%
-  as_tibble() %>%
-  select_if(is.numeric) %>%
-  gather(key = "variable", value = "value")
-
-data_key
-
-for (i in 1:length(clean_num)) {
-  print(ggplot(clean, aes(y = clean[,i], color = Status)) + 
-          geom_boxplot() + 
-          ylab(names(clean[i])) + 
-          theme(axis.title.x=element_blank(),
-                axis.text.x=element_blank(),
-                axis.ticks.x=element_blank()))
-}
+corrplot(corr)
+p_value_mat <- cor_pmat(clean_num)
+ggcorrplot(corr, type = "lower", p.mat = p_value_mat) 
 
 
 
+#----------Burata Analyses -----------------------------------------------
+clean$Status <- as.factor(clean$Status)
+dim(clean)
 
-#Correlation of Categorical values to Status
-# ---- Code here ---- 
+#----takes 10-15min!!!!------
+boruta_output <- Boruta(Status~., data = clean, doTrace=2)
+
+boruta_signif <- getSelectedAttributes(boruta_output, withTentative = TRUE)
+print(boruta_signif)
+
+plot(boruta_output, cex.axis=.7, las=2, xlab="", main="Variable Importance") 
+
+#---------------------------------------------------------------
+#---------------REMOVING UNSIGNIFICANT VARIABLES -----------------
+#---------------------------------------------------------------
 
 
-#Plot income to loan amount, interactive
-# ---- Code here ---- 
+Status <- clean$Status
+
+#Remove useless columns:
+features <- cbind(clean[boruta_signif], Status)
+
+dim(features)
+
+
+#----------------------------------------------------------------------------
+#--------- TRAIN THE MODEL---------------------------------------------------
+#----------------------------------------------------------------------------
+
+
+#--------Divide Sample in train and test ----------------------------
+set.seed(7)
+div <- createDataPartition(y = features$Status, p = 0.8, list = F)
+
+data.train <- features[div,]
+
+data.test <- features[-div,]
+
+# -----See the proportions -------------
+PercTable(features$Status)
+PercTable(data.train$Status)
+PercTable(data.test$Status)
+
+
+#All good!
+
+
+#Train classifier
+fit1 <- glm(Status ~ ., data=data.train,family=binomial())
+summary(fit1)
+
+
+#only show significant variables:
+significant.variables <- summary(fit1)$coeff[-1,4] < 0.05
+names(significant.variables)[significant.variables == TRUE]
+
+
+#Plot ROC
+data.test$fit1_score <- predict(fit1,type='response',data.test)
+fit1_pred <- prediction(data.test$fit1_score, data.test$Status)
+fit1_roc <- performance(fit1_pred, "tpr", "fpr")
+plot(fit1_roc, lwd=1, colorize = TRUE, main = "Fit1: Logit - ROC Curve")
+lines(x=c(0, 1), y=c(0, 1), col="black", lwd=1, lty=3)
+
+# --> could be better..
+
+#precision/ recall curve
+fit1_precision <- performance(fit1_pred, measure = "prec", x.measure = "rec")
+plot(fit1_precision, main="Fit1: Logit - Precision vs Recall")
+
+
+
+
+#Confusion matrix
+confusionMatrix(as.factor(round(data.test$fit1_score)), data.test$Status)
+
+#Accuracy could be better, now at 60%
+
+
+#AUC
+fit1_auc <- performance(fit1_pred, measure = "auc")
+cat("AUC: ",fit1_auc@y.values[[1]]*100)
 
 
 
 
 
-#--------------------------------------
-#--------- Exercise 2 -----------------
-#--------------------------------------
+
+
+#-------------------------------------------------------------------------------
+#--------- Reference: no cleaning of the data ----------------------------------
+#-------------------------------------------------------------------------------
+
+## Training and testing without any data quality checks and feature selection
+
+# Split the data 
+set.seed(7)
+data$Status <- as.factor(data$Status)
+div_2 <- createDataPartition(y = data$Status, p = 0.7, list = F)
+
+# Training Sample
+data.train_2 <- data[div,] # 70% here
+
+# Test Sample
+data.test_2 <- data[-div,] # rest of the 30% data goes here
+
+
+
+# Fit the model
+fit2 <- glm(Status ~ ., data=data.train_2,family=binomial())
+summary(fit2)
+
+
+
+
+# Extract and compare the significant variables 
+significant.variables_2 <- summary(fit2)$coeff[-1,4] < 0.05
+names(significant.variables)[significant.variables == TRUE]
+names(significant.variables_2)[significant.variables == TRUE]
+
+
+
+# Test the perfromance 
+data.test_2$fit2_score <- predict(fit2,type='response',data.test_2)
+fit2_pred <- prediction(data.test_2$fit2_score, data.test_2$Status)
+fit2_roc <- performance(fit2_pred, "tpr", "fpr")
+plot(fit2_roc, lwd=1, colorize = TRUE, main = "Fit2: Logit - ROC Curve")
+lines(x=c(0, 1), y=c(0, 1), col="black", lwd=1, lty=3)
+
+
+
+
+# Extract the confusion matrix 
+confusionMatrix(as.factor(round(data.test_2$fit2_score)), data.test_2$Status)
+
+
+
+# Extract the AUC value 
+fit2_auc <- performance(fit2_pred, measure = "auc")
+cat("AUC: ",fit2_auc@y.values[[1]]*100)
+
+
+
 
 
 
